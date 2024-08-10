a2659580d1a386eb27376225507f2364702b2b9d
Radosław Błędowski <rkbdi@aospa.co>
2024-08-11 01:03:18 +0530

    addons: Include Nostalgic System Icon Packs
    Ref: https://gerrit.aospa.co/c/AOSPA/android_vendor_aospa/+/40885
    
    Change-Id: I45b8c8ac7ca3449588dd69fa997b53ef20eee053
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -107,6 +107,11 @@ PRODUCT_PACKAGES += \
     IconPackRoundedSettingsOverlay \
     IconPackRoundedSystemUIOverlay \
     IconPackRoundedThemePickerOverlay \
+    IconPackNostalgicAndroidOverlay \
+    IconPackNostalgicLauncherOverlay \
+    IconPackNostalgicSettingsOverlay \
+    IconPackNostalgicSystemUIOverlay \
+    IconPackNostalgicThemePickerOverlay \
     IconPackOOSAndroidOverlay \
     IconPackOOSLauncherOverlay \
     IconPackOOSSettingsOverlay \
