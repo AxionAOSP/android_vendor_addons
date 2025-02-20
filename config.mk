e517b4343b05e56c320dfd5d6089c314c8bc91f3
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-20 08:35:23 +0800

    Ship Black theme
    Change-Id: I5643c6667e306bc06bb17ca74840084ff6f45576
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -27,6 +27,10 @@ PRODUCT_COPY_FILES += \
     vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2018-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2018-and-newer.xml \
     vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2019-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2019-and-newer.xml
 
+# Black Theme
+PRODUCT_PACKAGES += \
+    AndroidBlackThemeOverlay
+
 # Hide IME space
 PRODUCT_PACKAGES += \
     GesturalNavigationNarrowSpace \
